d43d0366330cd16767beee6b3fea875cfc428142
Crowdbotics <team@crowdbotics.com>
2020-06-02 00:43:13 +0000

    Update models

--- backend/fashion_icon_17683/wsgi.py
+++ backend/fashion_icon_17683/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fashion_icon_17683.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fashion_icon_17683.settings")
 
 application = get_wsgi_application()
